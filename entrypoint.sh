d8cf21cd0eaddbee488a9e9baa4773908f25f624
Hexel-Hosting <122264010+Hexel-Hosting@users.noreply.github.com>
2023-02-16 02:01:50 +0700

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,14 +15,14 @@ export INTERNAL_IP
 # Print Node.js Version
 cfonts "HEXEL-HOSTING" -a center -g blue,white
 neofetch
+printf "${RED}DOCKER VIP BY HEXEL - HOSTING"
+printf "${GREEN} [NODEJS V17 BERHASIL TERINSTALL"
+printf "${BLUE}INCLUDE PACKAGE: YARN, FFMPEG, IMAGEMAGICK, PYTHON, PM2, PNPM, NODEMON, SPEEDTEST"
 node -v
 
 # Replace Startup Variables
 # shellcheck disable=SC2086
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e "${RED}DOCKER VIP BY HEXEL - HOSTING"
-echo -e "${GREEN} [NODEJS V17 BERHASIL TERINSTALL"
-echo -e "${BLUE}INCLUDE PACKAGE: YARN, FFMPEG, IMAGEMAGICK, PYTHON, PM2, PNPM, NODEMON, SPEEDTEST"
 echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
 
 # Run the Server
