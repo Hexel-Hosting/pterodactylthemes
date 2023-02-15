52bccc5ec04fe406c02ecf7db99733cdf6bb2436
Hexel-Hosting <122264010+Hexel-Hosting@users.noreply.github.com>
2023-02-16 00:22:07 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,12 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Print Node.js Version
-cfonts "HEXEL-HOSTING" -a center -g blue
+cfonts "HEXEL-HOSTING" -a center -g blue,black,white
 neofetch
 
-echo "\033[33m [NODEJS V17 BY HEXEL HOSTING"
-echo "\033[0;34m INCLUDE PACKAGE: YARN, FFMPEG, IMAGEMAGICK, PYTHON, PM2, PNPM, NODEMON, SPEEDTEST"
-
+echo     "\033[31m DOCKER VIP BY HEXEL - HOSTING"
+echo     "\033[33m [NODEJS V17 BERHASIL TERINSTALL"
+echo     "\033[0;34m INCLUDE PACKAGE: YARN, FFMPEG, IMAGEMAGICK, PYTHON, PM2, PNPM, NODEMON, SPEEDTEST"
 node -v
 
 # Replace Startup Variables
